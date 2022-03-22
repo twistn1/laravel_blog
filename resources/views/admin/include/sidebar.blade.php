7b20c72ee1ced54bf1a6e9e4d63f244479858ca2
twistn1 <singoshkins@gmail.com>
2022-03-22 15:03:42 +0300

    first commit

---
+++ resources/views/admin/include/sidebar.blade.php
@@ -0,0 +1,7 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Sidebar -->
+    <div class="sidebar">
+    </div>
+    <!-- /.sidebar -->
+</aside>
