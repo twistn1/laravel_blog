f47f8df43e52ed7525497932c0eee0f9682352a3
twistn1 <singoshkins@gmail.com>
2022-03-23 20:06:58 +0300

    CRUD operations were added (posts)

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -5,6 +5,13 @@
 
         <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
+                <a href="{{ route('admin.post.index') }}" class="nav-link"><i class="nav-icon fas fa-clipboard"></i>
+                    <p class="ml-2">
+                        Посты
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
                 <a href="{{ route('admin.category.index') }}" class="nav-link"><i class="nav-icon fas fa-list-alt"></i>
                     <p class="ml-2">
                         Категории
