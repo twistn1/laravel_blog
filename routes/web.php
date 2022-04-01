623bf1a73d63df722e327cde17f6f79a8cf30af9
twistn1 <singoshkins@gmail.com>
2022-04-01 17:09:25 +0300

    email verification was added

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::group(['namespace' => 'Main'], function () {
     Route::get('/', 'IndexController');
 });
 
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' =>['auth', 'admin']], function () {
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' =>['auth', 'admin', 'verified']], function () {
     Route::group(['namespace' => 'Main'], function () {
         Route::get('/', 'IndexController');
         });
@@ -62,6 +62,6 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' =>['auth
     });
 });
 
-        Auth::routes();
+        Auth::routes(['verify' => true]);
 
 
