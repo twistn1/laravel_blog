1ab91149f022ec07f9f230167cf8fa1e7cb8506b
twistn1 <singoshkins@gmail.com>
2022-04-04 12:27:56 +0300

    added an option to list and delete liked posts

--- resources/views/personal/liked/index.blade.php
+++ resources/views/personal/liked/index.blade.php
@@ -23,6 +23,43 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
+                    <div class="col-6">
+                        <div class="card">
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Название</th>
+                                        <th colspan="2">Действия</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($posts as $post)
+                                        <tr>
+                                            <td>{{ $post->id }}</td>
+                                            <td>{{ $post->title }}</td>
+                                            <td><a href="{{ route('admin.post.show', $post->id) }}"><i
+                                                        class="far fa-eye"></i></a></td>
+                                            <td>
+                                                <form action="{{ route('personal.liked.delete', $post->id) }}"
+                                                      method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="border-0 bg-transparent">
+                                                        <i class="text-danger fas fa-trash" role="button"></i>
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                    @endforeach
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                    </div>
                 </div>
                 <!-- /.row -->
             </div><!-- /.container-fluid -->
